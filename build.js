4985533aba4872822488df2e6e3d3ed7f8bd15ec
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-24 08:29:51 +0100

    yo phovea:update

--- build.js
+++ build.js
@@ -47,19 +47,13 @@ function fromRepoUrl(url) {
  */
 function spawn(cmd, args, opts) {
   const spawn = require('child_process').spawn;
+  const _ = require('lodash');
   return new Promise((resolve, reject) => {
-    const p = spawn(cmd, args, opts);
-    if (!quiet) {
-      p.stdout.on('data', (data) => console.log(data.toString()));
-      p.stderr.on('data', (data) => console.error(chalk.red(data.toString())));
-    }
-	p.on('error', (error) => {
-		console.error(error);
-	});
-    p.on('exit', (code, signal) => {
+    const p = spawn(cmd, args, _.merge({stdio: ['ignore', 1, 2]}, opts));
+    p.on('close', (code, signal) => {
       if (code === 0) {
         console.info(cmd, 'ok status code',code, signal);
-        resolve(code)
+        resolve(code);
       } else {
         console.error(cmd, 'status code',code, signal);
         reject(`${cmd} failed with status code ${code} ${signal}`);
